e34492d010ff2c246e1bd422d14d9eb456433f8c
Hanming Hu <hhu@ucsd.edu>
2021-11-14 20:19:52 -0800

    Labsubmission

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Lab 8 - Starter
+# Hanming Hu
+
+### 1) I'll choose the first option: Within a Github action that runs whenever code is pushed. The reason is we can check if there is an error for every update and it execute quickly. Running them all after development is completed cannot let us know which specific part goes wrong, and manual running test is not part of automated tests.
+
+### 2) No.
+
+### 3) I would not use a unit test to test the “message” feature, because it might contains multiple components (like write function and send function), which makes it hard to do unit testing.
+
+### 4) Yes. Because just checking the message length is an independent function, which can be tested using unit test.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,71 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+test('test if it is a valid phone number', () => {
+    expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+});
+test('test if it is a valid phone number', () => {
+    expect(functions.isPhoneNumber('987-654-3210')).toBe(true);
+});
+test('test if it is an invalid phone number', () => {
+    expect(functions.isPhoneNumber('Hello')).toBe(false);
+});
+test('test if it is an invalid phone number', () => {
+    expect(functions.isPhoneNumber('2934029234')).toBe(false);
+});
+
+
+test('test if it is a valid email', () => {
+    expect(functions.isEmail('FirstName@ucsd.edu')).toBe(true);
+});
+test('test if it is a valid email', () => {
+    expect(functions.isEmail('LastName@gmail.com')).toBe(true);
+});
+test('test if it is an invalid email', () => {
+    expect(functions.isEmail('344-343-2322')).toBe(false);
+});
+test('test if it is an invalid email', () => {
+    expect(functions.isEmail('wrgiuuhiowu')).toBe(false);
+});
+
+
+test('test if it is a strong password', () => {
+    expect(functions.isStrongPassword('wrgiuuhiowu')).toBe(true);
+});
+test('test if it is a strong password', () => {
+    expect(functions.isStrongPassword('sdfsddgiudfs')).toBe(true);
+});
+test('test if it is a weak password', () => {
+    expect(functions.isStrongPassword('89aisjpufia')).toBe(false);
+});
+test('test if it is a weak password', () => {
+    expect(functions.isStrongPassword('abc')).toBe(false);
+});
+
+
+test('test if it is a valid date', () => {
+    expect(functions.isDate('01/01/0001')).toBe(true);
+});
+test('test if it is a valid date', () => {
+    expect(functions.isDate('99/99/9999')).toBe(true);
+});
+test('test if it is an invalid date', () => {
+    expect(functions.isDate('December 12th')).toBe(false);
+});
+test('test if it is an invalid date', () => {
+    expect(functions.isDate('9999/9999/9999')).toBe(false);
+});
+
+
+test('test if it is a hexcolor', () => {
+    expect(functions.isHexColor('abcdef')).toBe(true);
+});
+test('test if it is a hexcolor', () => {
+    expect(functions.isHexColor('bad')).toBe(true);
+});
+test('test if it is not a hexcolor', () => {
+    expect(functions.isHexColor('abcdefghijklmnopqrstuvwxyz')).toBe(false);
+});
+test('test if it is not a hexcolor', () => {
+    expect(functions.isHexColor('2')).toBe(false);
+});
